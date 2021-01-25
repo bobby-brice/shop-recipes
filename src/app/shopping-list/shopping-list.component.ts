084a9f8e0918e1382852c7f01427df8eda6caeaf
Bobby Brice <bobby.brice@gmail.com>
2021-01-24 21:25:28 -0500

    Add ingredient inputs and buttons

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -8,7 +8,7 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
-    new Ingredient('Applies', 5),
+    new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
   ];
   constructor() { }
